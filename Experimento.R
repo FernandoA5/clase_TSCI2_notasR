799f7977866c8581cf9d0d4fb059d87b7dc0a67d
Fernando Alcalá <fernandoalcalacasas@hotmail.com>
2023-02-24 15:20:09 -0600

    Experimento con nota Do5

--- Experimento.R
+++ Experimento.R
@@ -1,15 +1,21 @@
+library("tuneR")
+library("seewave")
+library("stats")
+
 
 #EXPERIMENTO 
-do_pecho <- readWave(file.choose())
-do_pecho
+do5<- readWave(file.choose()) 
+do5
+do5_oscillo <- oscillo(do5, title="Oscillogram DO5")
 
-oscillo(do_pecho, title="Ocillograma Do de Pecho")
-do_pecho_per <- periodogram(do_pecho)
-plot(do_pecho_per, main="Periodogram Do de Pecho")
+do5_per <- periodogram(do5, main="Periodogram DO5")
+plot(do5_per)
+do5
+do5.downsamped <- downsample(do5, samp.rate =24000)
 
+do5_spec <- spec(do5.downsamped)
+plot(do5_spec)
 
-sol <- readWave(file.choose())
-sol
-oscillo(sol, title="Oscillograma Sol")
-sol_per <- periodogram(sol)
-plot(sol_per, main="Periodogram Sol")
+dfDo5 <- as.data.frame(do5_spec)
+max(dfDo5)
+min(dfDo5)
